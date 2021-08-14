75db65b590839a3c7d6f7840f9be500a4190814d
rukiii4444 <haruki14rezer@gmail.com>
2021-08-14 22:49:51 +0900

    user一覧, 詳細ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  #get 'home/top'
+  get "users/index" => "users#index"
+  get "users/:id" => "users#show"
+ 
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
